1cad2bda57e6955620311041ae2e25bcb3c9786a
Alexandre <alascenci@gmail.com>
2020-02-25 23:27:01 +0100

    Optimisation code AstreLumineux.

--- src/scene-components/SystemeSolaire/abstract/AstreLumineux.js
+++ src/scene-components/SystemeSolaire/abstract/AstreLumineux.js
@@ -5,13 +5,7 @@ function AstreLumineux(props) {
     return (
         <group position={props.position}>
             <pointLight specular={props.specular} position={props.position} />
-            <Astre
-                position={props.position}
-                material={props.material}
-                radius={props.radius}
-                precision={props.precision}
-                rotation={props.rotation}
-            />
+            <Astre {...props} />
         </group>
     )
 }
