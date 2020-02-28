b879d03085f48ad3dc184de96bbb860e0bbbd2bf
Alexandre <alascenci@gmail.com>
2020-02-28 15:19:51 +0100

    Intégration caméra.

--- src/scenes/Scene.js
+++ src/scenes/Scene.js
@@ -1,15 +1,31 @@
 import React, {Component} from "react";
 import {Canvas} from "react-three-fiber";
 import utils from "../utils";
+import {PerspectiveCamera} from "three";
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 
 class Scene extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            camera: new PerspectiveCamera(
+                45,
+                window.innerWidth / window.innerHeight,
+                1,
+                4000
+            )
+        }
+    }
+
     componentDidMount(): void {
         document.title = utils.config.getConfigScene(this._reactInternalFiber.return.elementType.name).titre;
+        this.state.camera.position.set(0, 0, 20);
+        this.state.camera.lookAt(0, 0, 0);
     }
 
     render() {
         return (
-            <Canvas style={this.props.style}>
+            <Canvas camera={this.state.camera} style={this.props.style}>
                 {this.props.children}
             </Canvas>
         );
